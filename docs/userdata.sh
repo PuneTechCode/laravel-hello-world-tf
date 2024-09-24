dde4093c42d82aebcdf8a73255d083b94c4f7d6a
Rajdattpawar <rajpawar02@gmail.com>
2024-09-24 23:15:50 +0530

    upda

--- docs/userdata.sh
+++ docs/userdata.sh
@@ -1,10 +1,54 @@
 #!/bin/bash
 
 yum update -y 
-sudo yum install -y php php-mbstring php-intl
+sudo yum install -y php php-mbstring php-intl nginx
+
 curl -sS https://getcomposer.org/installer | php
 mv composer.phar /usr/local/bin/composer
 composer install -n || true
+
+yum install 
+systemctl start nginx
+systemctl enable nginx
+
+cat <<EOL > /etc/nginx/conf.d/laravel.conf
+server {
+    listen 80;
+    server_name 0.0.0.0;
+    root /var/www/html/laravel-hello-world/public;
+
+    add_header X-Frame-Options "SAMEORIGIN";
+    add_header X-XSS-Protection "1; mode=block";
+    add_header X-Content-Type-Options "nosniff";
+
+    index index.html index.htm index.php;
+
+    charset utf-8;
+
+    location / {
+        try_files \$uri \$uri/ /index.php?\$query_string;
+    }
+
+    location = /favicon.ico { access_log off; log_not_found off; }
+    location = /robots.txt  { access_log off; log_not_found off; }
+
+    error_page 404 /index.php;
+
+    location ~ \.php$ {
+        fastcgi_pass unix:/var/run/php-fpm/www.sock;
+        fastcgi_index index.php;
+        fastcgi_param SCRIPT_FILENAME \$realpath_root\$fastcgi_script_name;
+        include fastcgi_params;
+    }
+
+    location ~ /\.(?!well-known).* {
+        deny all;
+    }
+}
+EOL
+
+systemctl restart nginx
+
 ssm_parameter_value=$(aws ssm get-parameter --name "public_key" --query "Parameter.Value" --output text)
 
 # Append the SSM parameter value to the authorized_keys file
